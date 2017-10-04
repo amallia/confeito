e8dd345b1d697a76bb5ae3d21c88459dfb58f51b
Tatsuhiro Aoshima <hiro4bbh@gmail.com>
2017-10-04 13:58:41 +0900

    Use github.com/hiro4bbh/go-assert

--- feature_test.go
+++ feature_test.go
@@ -1,33 +1,37 @@
 package confeito
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/hiro4bbh/go-assert"
+)
 
 func TestDenseFeature(t *testing.T) {
 	x := DenseFeature{-2.0, -1.0, 0.0, 1.0, 2.0}
-	NewAssert(t, float32(-2.0)).EqualWithoutError(x.Get(0))
-	NewAssert(t, float32(-1.0)).EqualWithoutError(x.Get(1))
-	NewAssert(t, float32(0.0)).EqualWithoutError(x.Get(2))
-	NewAssert(t, float32(1.0)).EqualWithoutError(x.Get(3))
-	NewAssert(t, float32(2.0)).EqualWithoutError(x.Get(4))
-	NewAssert(t, "id 5 is out of range \\[0:5\\]").ExpectError(x.Get(5))
-	NewAssert(t, "id must be legal one").ExpectError(x.Get(_FEATURE_ID_ILLEGAL))
-	NewAssert(t, 5).Equal(x.Size())
+	goassert.New(t, float32(-2.0)).EqualWithoutError(x.Get(0))
+	goassert.New(t, float32(-1.0)).EqualWithoutError(x.Get(1))
+	goassert.New(t, float32(0.0)).EqualWithoutError(x.Get(2))
+	goassert.New(t, float32(1.0)).EqualWithoutError(x.Get(3))
+	goassert.New(t, float32(2.0)).EqualWithoutError(x.Get(4))
+	goassert.New(t, "id 5 is out of range \\[0:5\\]").ExpectError(x.Get(5))
+	goassert.New(t, "id must be legal one").ExpectError(x.Get(_FEATURE_ID_ILLEGAL))
+	goassert.New(t, 5).Equal(x.Size())
 }
 
 func TestSparseFeature(t *testing.T) {
 	x := NewSparseFeature(5)
-	NewAssert(t).SucceedWithoutError(x.Set(0, -2.0))
-	NewAssert(t).SucceedWithoutError(x.Set(1, -1.0))
-	NewAssert(t).SucceedWithoutError(x.Set(2, 0.0))
-	NewAssert(t).SucceedWithoutError(x.Set(4, 2.0))
-	NewAssert(t, "id 5 is out of range \\[0:5\\]").ExpectError(x.Set(5, 3.0))
-	NewAssert(t, "id must be legal one").ExpectError(x.Set(_FEATURE_ID_ILLEGAL, 3.0))
-	NewAssert(t, float32(-2.0)).EqualWithoutError(x.Get(0))
-	NewAssert(t, float32(-1.0)).EqualWithoutError(x.Get(1))
-	NewAssert(t, float32(0.0)).EqualWithoutError(x.Get(2))
-	NewAssert(t, float32(0.0)).EqualWithoutError(x.Get(3))
-	NewAssert(t, float32(2.0)).EqualWithoutError(x.Get(4))
-	NewAssert(t, "id 5 is out of range \\[0:5\\]").ExpectError(x.Get(5))
-	NewAssert(t, "id must be legal one").ExpectError(x.Get(_FEATURE_ID_ILLEGAL))
-	NewAssert(t, 5).Equal(x.Size())
+	goassert.New(t).SucceedWithoutError(x.Set(0, -2.0))
+	goassert.New(t).SucceedWithoutError(x.Set(1, -1.0))
+	goassert.New(t).SucceedWithoutError(x.Set(2, 0.0))
+	goassert.New(t).SucceedWithoutError(x.Set(4, 2.0))
+	goassert.New(t, "id 5 is out of range \\[0:5\\]").ExpectError(x.Set(5, 3.0))
+	goassert.New(t, "id must be legal one").ExpectError(x.Set(_FEATURE_ID_ILLEGAL, 3.0))
+	goassert.New(t, float32(-2.0)).EqualWithoutError(x.Get(0))
+	goassert.New(t, float32(-1.0)).EqualWithoutError(x.Get(1))
+	goassert.New(t, float32(0.0)).EqualWithoutError(x.Get(2))
+	goassert.New(t, float32(0.0)).EqualWithoutError(x.Get(3))
+	goassert.New(t, float32(2.0)).EqualWithoutError(x.Get(4))
+	goassert.New(t, "id 5 is out of range \\[0:5\\]").ExpectError(x.Get(5))
+	goassert.New(t, "id must be legal one").ExpectError(x.Get(_FEATURE_ID_ILLEGAL))
+	goassert.New(t, 5).Equal(x.Size())
 }
--- leaf_test.go
+++ leaf_test.go
@@ -3,21 +3,23 @@ package confeito
 import (
 	"fmt"
 	"testing"
+
+	"github.com/hiro4bbh/go-assert"
 )
 
 func TestTerminalLeaf(t *testing.T) {
 	x := DenseFeature{-2.0, -1.0, 0.0, 1.0, 2.0}
 
-	terminal1 := NewAssert(t).SucceedNew(NewTerminalLeaf(float32(1.0))).(*Leaf)
-	NewAssert(t, (*Leaf)(nil)).Equal(terminal1.GetLeft())
-	NewAssert(t, (*Leaf)(nil)).Equal(terminal1.GetRight())
-	NewAssert(t, "terminal leaf does not have threshold").ExpectError(terminal1.GetThreshold())
-	NewAssert(t, float32(1.0)).EqualWithoutError(terminal1.GetValue())
-	NewAssert(t, true).Equal(terminal1.IsTerminal())
-	NewAssert(t, float32(1.0)).EqualWithoutError(terminal1.Predict(x))
-	NewAssert(t, "terminal leaf cannot have left leaf").ExpectError(terminal1.SetLeft(terminal1))
-	NewAssert(t, "terminal leaf cannot have right leaf").ExpectError(terminal1.SetRight(terminal1))
-	NewAssert(t, "1").Equal(fmt.Sprintf("%s", terminal1))
+	terminal1 := goassert.New(t).SucceedNew(NewTerminalLeaf(float32(1.0))).(*Leaf)
+	goassert.New(t, (*Leaf)(nil)).Equal(terminal1.GetLeft())
+	goassert.New(t, (*Leaf)(nil)).Equal(terminal1.GetRight())
+	goassert.New(t, "terminal leaf does not have threshold").ExpectError(terminal1.GetThreshold())
+	goassert.New(t, float32(1.0)).EqualWithoutError(terminal1.GetValue())
+	goassert.New(t, true).Equal(terminal1.IsTerminal())
+	goassert.New(t, float32(1.0)).EqualWithoutError(terminal1.Predict(x))
+	goassert.New(t, "terminal leaf cannot have left leaf").ExpectError(terminal1.SetLeft(terminal1))
+	goassert.New(t, "terminal leaf cannot have right leaf").ExpectError(terminal1.SetRight(terminal1))
+	goassert.New(t, "1").Equal(fmt.Sprintf("%s", terminal1))
 }
 
 func TestLeaf(t *testing.T) {
@@ -28,24 +30,24 @@ func TestLeaf(t *testing.T) {
 	x5 := DenseFeature{-2.0, 1.0, 0.0, 2.0, 3.0}
 	x6 := DenseFeature{-2.0, 1.0, 0.0, 2.0}
 
-	NewAssert(t, "featureID must be valid").ExpectError(NewLeaf(_FEATURE_ID_ILLEGAL, 0.0, nil, nil))
+	goassert.New(t, "featureID must be valid").ExpectError(NewLeaf(_FEATURE_ID_ILLEGAL, 0.0, nil, nil))
 
-	leaf1 := NewAssert(t).SucceedNew(NewLeaf(1, -0.5, float32(1.0), float32(2.0))).(*Leaf)
-	NewAssert(t, float32(1.0)).EqualWithoutError(leaf1.GetLeft().GetValue())
-	NewAssert(t, float32(2.0)).EqualWithoutError(leaf1.GetRight().GetValue())
-	NewAssert(t, FeatureID(1), float32(-0.5)).EqualWithoutError(leaf1.GetThreshold())
-	NewAssert(t, "non-terminal leaf does not have value").ExpectError(leaf1.GetValue())
-	NewAssert(t, false).Equal(leaf1.IsTerminal())
-	NewAssert(t, float32(1.0)).EqualWithoutError(leaf1.Predict(x1))
-	NewAssert(t, "left leaf of non-terminal leaf must not be nil").ExpectError(leaf1.SetLeft(nil))
-	NewAssert(t, "right leaf of non-terminal leaf must not be nil").ExpectError(leaf1.SetRight(nil))
+	leaf1 := goassert.New(t).SucceedNew(NewLeaf(1, -0.5, float32(1.0), float32(2.0))).(*Leaf)
+	goassert.New(t, float32(1.0)).EqualWithoutError(leaf1.GetLeft().GetValue())
+	goassert.New(t, float32(2.0)).EqualWithoutError(leaf1.GetRight().GetValue())
+	goassert.New(t, FeatureID(1), float32(-0.5)).EqualWithoutError(leaf1.GetThreshold())
+	goassert.New(t, "non-terminal leaf does not have value").ExpectError(leaf1.GetValue())
+	goassert.New(t, false).Equal(leaf1.IsTerminal())
+	goassert.New(t, float32(1.0)).EqualWithoutError(leaf1.Predict(x1))
+	goassert.New(t, "left leaf of non-terminal leaf must not be nil").ExpectError(leaf1.SetLeft(nil))
+	goassert.New(t, "right leaf of non-terminal leaf must not be nil").ExpectError(leaf1.SetRight(nil))
 
-	leaf2 := NewAssert(t).SucceedNew(NewLeaf(2, 0.5, float32(1.0), float32(2.0))).(*Leaf)
-	leaf3 := NewAssert(t).SucceedNew(NewLeaf(3, 1.5, float32(3.0), float32(4.0))).(*Leaf)
-	leaf4 := NewAssert(t).SucceedNew(NewLeaf(4, 2.5, float32(4.0), float32(5.0))).(*Leaf)
-	NewAssert(t).SucceedWithoutError(leaf1.SetLeft(leaf2))
-	NewAssert(t).SucceedWithoutError(leaf1.SetRight(leaf3))
-	NewAssert(t).SucceedWithoutError(leaf3.SetRight(leaf4))
+	leaf2 := goassert.New(t).SucceedNew(NewLeaf(2, 0.5, float32(1.0), float32(2.0))).(*Leaf)
+	leaf3 := goassert.New(t).SucceedNew(NewLeaf(3, 1.5, float32(3.0), float32(4.0))).(*Leaf)
+	leaf4 := goassert.New(t).SucceedNew(NewLeaf(4, 2.5, float32(4.0), float32(5.0))).(*Leaf)
+	goassert.New(t).SucceedWithoutError(leaf1.SetLeft(leaf2))
+	goassert.New(t).SucceedWithoutError(leaf1.SetRight(leaf3))
+	goassert.New(t).SucceedWithoutError(leaf3.SetRight(leaf4))
 	// (feature[1] <= -0.5
 	//     ? (feature[2] <= 0.5 ? 1 : 2)
 	//     : (feature[3] <= 1.5
@@ -53,11 +55,11 @@ func TestLeaf(t *testing.T) {
 	//           : (feature[4] <= 2.5 ? 4 : 5)
 	//       )
 	// )
-	NewAssert(t, "(feature[1] <= -0.5 ? (feature[2] <= 0.5 ? 1 : 2) : (feature[3] <= 1.5 ? 3 : (feature[4] <= 2.5 ? 4 : 5)))").Equal(fmt.Sprintf("%s", leaf1))
-	NewAssert(t, float32(1.0)).EqualWithoutError(leaf1.Predict(x1))
-	NewAssert(t, float32(2.0)).EqualWithoutError(leaf1.Predict(x2))
-	NewAssert(t, float32(3.0)).EqualWithoutError(leaf1.Predict(x3))
-	NewAssert(t, float32(4.0)).EqualWithoutError(leaf1.Predict(x4))
-	NewAssert(t, float32(5.0)).EqualWithoutError(leaf1.Predict(x5))
-	NewAssert(t, "id 4 is out of range").ExpectError(leaf1.Predict(x6))
+	goassert.New(t, "(feature[1] <= -0.5 ? (feature[2] <= 0.5 ? 1 : 2) : (feature[3] <= 1.5 ? 3 : (feature[4] <= 2.5 ? 4 : 5)))").Equal(fmt.Sprintf("%s", leaf1))
+	goassert.New(t, float32(1.0)).EqualWithoutError(leaf1.Predict(x1))
+	goassert.New(t, float32(2.0)).EqualWithoutError(leaf1.Predict(x2))
+	goassert.New(t, float32(3.0)).EqualWithoutError(leaf1.Predict(x3))
+	goassert.New(t, float32(4.0)).EqualWithoutError(leaf1.Predict(x4))
+	goassert.New(t, float32(5.0)).EqualWithoutError(leaf1.Predict(x5))
+	goassert.New(t, "id 4 is out of range").ExpectError(leaf1.Predict(x6))
 }
